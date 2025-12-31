ad3fc48ae53e11844161f60cb28d53a236bbcdb4
Bhuvanesh <bhuvanprabha4@gmail.com>
2025-12-31 19:34:25 +0530

    Add PalindromeCheck class to check for palindromes

---
+++ .java
@@ -0,0 +1,21 @@
+package Simple;
+
+public class PalindromeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 121, temp = num, rev = 0;
+
+		while (temp != 0) {
+		    rev = rev * 10 + temp % 10;
+		    temp = temp / 10;
+		}
+
+		if (num == rev)
+		    System.out.println("Palindrome");
+		else
+		    System.out.println("Not Palindrome");
+
+	}
+
+}
